7b3e4e8276c51446e89eb075fb5644f4216057aa
Rene Sanabria <rsanabria@foodlogiq.com>
2022-08-12 19:18:56 -0600

    [] - Agregar text input

--- App.tsx
+++ App.tsx
@@ -1,23 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  SafeAreaView,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import { TextInput } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { TailwindProvider } from 'tailwindcss-react-native';
-import { Task, TaskData } from './src/components';
-import { Loading } from './src/components/loading';
 import { Tasklist } from './src/components/task-list';
 
 export default function App() {
+  const [name, setName] = useState('');
+
   return (
     <TailwindProvider>
-      <SafeAreaProvider>
+      <SafeAreaProvider className="bg-indigo-400">
         <StatusBar style="auto" />
-        {/* <Tasklist /> */}
-        <Loading />
+        <SafeAreaView className="flex-1">
+          <TextInput
+            placeholder="Nombre"
+            className="bg-white p-2 border text-lg"
+            onChangeText={(text) => setName(text)}
+            value={name}
+          />
+          <Text>{name}</Text>
+          <Tasklist />
+        </SafeAreaView>
       </SafeAreaProvider>
     </TailwindProvider>
   );
